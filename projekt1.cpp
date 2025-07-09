ce1a72490f47fb918e9d2f766f8f0f20e7f43415
Rtxip <rtxip@mat.umk.pl>
2025-07-09 12:34:27 +0200

    dodono menu z funkacja silnia

--- projekt1.cpp
+++ projekt1.cpp
@@ -1,4 +1,19 @@
+#include <iostream>
+using namespace std;
+
 int silnia(int n){
 	if(n<=1)return 1;
 	return n*silnia(n-1);
 }
+
+int main(){
+	int w;
+	cout<<"1. Oblicz silnie"<<endl;
+	cin >> w;
+
+	if(w==1){
+		int l;
+		cin>>l;
+		cout<<silnia(l)<<endl;
+	}
+	return 0;
